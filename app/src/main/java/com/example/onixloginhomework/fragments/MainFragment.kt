35773970ccf55efb41f198f55dcdc27540e47de7
Boonapart228 <kolyha101@gmail.com>
2022-06-01 19:55:31 +0300

    initial commit

---
+++ app/src/main/java/com/example/onixloginhomework/fragments/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.example.onixloginhomework.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.onixloginhomework.R
+import com.example.onixloginhomework.databinding.FragmentLoginBinding
+import com.example.onixloginhomework.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private val navArgs: MainFragmentArgs by navArgs()
+
+    lateinit var binding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.user.text = navArgs.loginhello
+
+    }
+}
