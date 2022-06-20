a0b0fdfbbdcfab510af3908c97e4ee92a72b6122
Boonapart228 <kolyha101@gmail.com>
2022-06-20 16:24:47 +0300

    lesson_four

--- app/src/main/java/com/example/onixloginhomework/di/App.kt
+++ app/src/main/java/com/example/onixloginhomework/di/App.kt
@@ -1,12 +1,10 @@
 package com.example.onixloginhomework.di
 
 import com.example.onixloginhomework.fragments.splash.SplashViewModel
-import com.example.onixloginhomework.settings.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val app = module {
 
     viewModel<SplashViewModel> { SplashViewModel() }
-    viewModel<SettingsViewModel> { SettingsViewModel() }
 }
--- app/src/main/java/com/example/onixloginhomework/settings/SettingsModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.onixloginhomework.settings
-
-class SettingsModel {
-    var checkChoice :Boolean = false
-    var position : Int = 0
-}
